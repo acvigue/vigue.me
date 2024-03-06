aab28559ca937855e1dc6be167d1c896cb53335e
Aiden Vigue <aiden@vigue.me>
2024-03-06 16:59:57 -0500

    bg before canvas load

--- layouts/GLLayout.vue
+++ layouts/GLLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative text-gray-200">
-    <WebGLView style="pointer-events: all !important" class="z-5" />
+    <WebGLView style="pointer-events: all !important" class="z-5 bg-gray-900" />
     <NavBar class="z-20" />
     <slot />
     <ScrollIndicator class="text-gray-200 z-10 pointer-events-none" />
