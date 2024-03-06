f31a6a1d9e33cc0b0758fa514ff545cb4b26d8c3
Aiden Vigue <aiden@vigue.me>
2024-03-06 17:06:14 -0500

    fix flash

--- layouts/GLLayout.vue
+++ layouts/GLLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="relative text-gray-200">
-    <WebGLView style="pointer-events: all !important" class="z-5 bg-gray-900" />
+    <div class="fixed bg-slate-950 w-screen h-screen z-4"></div>
+    <WebGLView style="pointer-events: all !important" class="z-5" />
     <NavBar class="z-20" />
     <slot />
     <ScrollIndicator class="text-gray-200 z-10 pointer-events-none" />
