c0df7f84ea0b1a0f6208b625b83d0b91d08a89d1
Aiden Vigue <aiden@vigue.me>
2024-03-31 18:53:43 -0400

    no co

--- components/WebGL/View.vue
+++ components/WebGL/View.vue
@@ -1,26 +1,24 @@
 <template>
-  <ClientOnly>
-    <TresCanvas
-      clear-color="#020617"
-      :window-size="true"
-      class="fixed top-0 left-0 w-screen h-screen"
-      :use-window-pointer="true"
-    >
-      <TresPerspectiveCamera :position="[0, 0, 5]" :look-at="[0, 0, 0]" />
-      <!--<OrbitControls />-->
-      <Stars
-        :rotation="[0, starsYRotation, 0]"
-        :radius="50"
-        :depth="50"
-        :count="5000"
-        :size="0.2"
-        :size-attenuation="true"
-      />
+  <TresCanvas
+    clear-color="#020617"
+    :window-size="true"
+    class="fixed top-0 left-0 w-screen h-screen"
+    :use-window-pointer="true"
+  >
+    <TresPerspectiveCamera :position="[0, 0, 5]" :look-at="[0, 0, 0]" />
+    <!--<OrbitControls />-->
+    <Stars
+      :rotation="[0, starsYRotation, 0]"
+      :radius="50"
+      :depth="50"
+      :count="5000"
+      :size="0.2"
+      :size-attenuation="true"
+    />
 
-      <TresAmbientLight :intensity="1" />
-      <WebGLGalaxy />
-    </TresCanvas>
-  </ClientOnly>
+    <TresAmbientLight :intensity="1" />
+    <WebGLGalaxy />
+  </TresCanvas>
 </template>
 
 <script setup lang="ts">
