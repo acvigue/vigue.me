f3df3f9e4b9cb0432b656c4440c46b16d1d4bad1
Aiden Vigue <aiden@vigue.me>
2024-04-04 18:56:27 -0400

    shake formkit

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,16 +1,21 @@
 // formkit.config.ts
 import { en } from '@formkit/i18n'
 import { defineFormKitConfig } from '@formkit/vue'
-import { genesisIcons } from '@formkit/icons'
+import { form, text, textarea } from '@formkit/inputs'
+
 import { rootClasses } from './formkit.theme'
 
 export default defineFormKitConfig(() => {
   return {
     locales: { en },
     locale: 'en',
+    inputs: {
+      textarea,
+      text,
+      form,
+    },
     config: {
       rootClasses,
-      ...genesisIcons,
     },
   }
 })
