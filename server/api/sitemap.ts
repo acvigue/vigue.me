3573f64cff132e1187d1cf106035ffe6db3301fe
Aiden Vigue <aiden@vigue.me>
2024-06-28 15:30:46 -0400

    sitemap optimize

--- server/api/sitemap.ts
+++ server/api/sitemap.ts
@@ -12,17 +12,8 @@ export async function getAllPosts() {
   const response = await api.posts
     .browse({ limit: 10, page: 1 })
     .fields({
-      id: true,
       slug: true,
       updated_at: true,
-      published_at: true,
-      meta_description: true,
-      meta_title: true,
-      title: true,
-      og_description: true,
-      og_image: true,
-      feature_image: true,
-      excerpt: true,
     })
     .fetch()
   if (!response.success) {
@@ -36,17 +27,8 @@ export async function getAllPosts() {
     const tempResponse = await api.posts
       .browse({ limit: 10, page: i, filter: 'status:published' })
       .fields({
-        id: true,
         slug: true,
         updated_at: true,
-        published_at: true,
-        meta_description: true,
-        meta_title: true,
-        title: true,
-        og_description: true,
-        og_image: true,
-        feature_image: true,
-        excerpt: true,
       })
       .fetch()
     if (!tempResponse.success)
@@ -60,22 +42,76 @@ export async function getAllPosts() {
   return { posts: response.data, pagination: response.meta.pagination }
 }
 
+export async function getAllPages() {
+  const config = useRuntimeConfig()
+
+  const api = new TSGhostAdminAPI(
+    config.ghostUrl,
+    config.ghostAdminKey,
+    'v5.47.0',
+  )
+
+  const response = await api.pages
+    .browse({ limit: 10, page: 1 })
+    .fields({
+      slug: true,
+      updated_at: true,
+    })
+    .fetch()
+  if (!response.success) {
+    const errorString = response.errors
+      .flatMap(error => error.message)
+      .join(', ')
+    throw new Error(errorString)
+  }
+
+  for (let i = 2; i <= response.meta.pagination.pages; i++) {
+    const tempResponse = await api.pages
+      .browse({ limit: 10, page: i, filter: 'status:published' })
+      .fields({
+        slug: true,
+        updated_at: true,
+      })
+      .fetch()
+    if (!tempResponse.success)
+      throw new Error(tempResponse.errors.join(', '))
+
+    tempResponse.data.forEach((p) => {
+      response.data.push(p)
+    })
+  }
+
+  return { pages: response.data, pagination: response.meta.pagination }
+}
+
 export default defineSitemapEventHandler(async (_e) => {
   const allPosts = await getAllPosts()
+  const allPages = await getAllPages()
 
-  const locs = allPosts.posts.map((p) => {
+  const postLocations = allPosts.posts.map((p) => {
     return {
       loc: `/posts/${p.slug}`,
       lastmod: p.updated_at ?? undefined,
     }
   })
 
-  for (let i = 1; i <= allPosts.pagination.pages; i++) {
-    locs.push({
-      loc: `/posts/${i}`,
-      lastmod: undefined
-    })
-  }
+  const postPages = Array(allPosts.pagination.pages).fill(0).map((_, i) => {
+    return {
+      loc: `/posts/${i + 1}`,
+      lastmod: undefined,
+    }
+  })
 
-  return locs
+  const pageLocations = allPages.pages.map((p) => {
+    return {
+      loc: `/${p.slug}`,
+      lastmod: p.updated_at ?? undefined,
+    }
+  })
+
+  return [
+    ...postLocations,
+    ...postPages,
+    ...pageLocations,
+  ]
 })
