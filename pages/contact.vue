8a3e8b1b6f7e9474a2e0338aa8519811903a8959
Aiden Vigue <aiden@vigue.me>
2024-06-28 21:24:15 -0400

    contact page complete

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,15 +2,18 @@
   <WidthContainer>
     <div class="flex justify-center items-center w-full py-[4rem]">
       <div class="flex flex-col h-full justify-center gap-12 w-full">
-        <div class="flex flex-1 justify-start items-center gap-2">
-          <span class="font-serif italic md:text-7xl text-4xl text-persian font-bold">Let's</span>
-          <span class="font-serif italic md:text-6xl text-4xl mt-8">Connect</span>
+        <div class="flex flex-1 justify-start items-center gap-2 lg:flex-row flex-col">
+          <span class="font-serif italic lg:text-6xl text-4xl text-persian font-bold lg:ml-0 -ml-32">Got an
+            idea?</span>
+          <span class="font-serif italic lg:text-5xl text-3xl lg:mt-8 mt-0 lg:pl-0 pl-20">Let's bring it to life!</span>
         </div>
         <div class="w-full flex justify-center">
           <div class="rounded-lg p-4 border border-khaki shadow-lg w-full max-w-4xl">
+            <h4 class="mb-4 text-2xl font-serif text-persian font-bold">Contact Me</h4>
             <FormKit type="form" class="lg:w-2xl" :actions="false">
-              <FormKit type="text" label="Name" validation="required" />
+              <FormKit type="text" label="Your Name" validation="required" />
               <FormKit type="text" label="Email" validation="required|email" />
+              <FormKit type="text" label="Employer" validation="required" />
               <FormKit type="textarea" label="Message" validation="required" />
               <button type="submit"
                 class="flex text-licorice items-center gap-2 border-licorice border-2 rounded-full px-4 hover:bg-licorice hover:text-champagne duration-500"
@@ -21,8 +24,6 @@
             </FormKit>
           </div>
         </div>
-
-        <div class="flex-1" />
       </div>
     </div>
   </WidthContainer>
@@ -42,4 +43,12 @@ useSeoMeta({
   ogUrl: `${appConfig.baseUrl}/contact`,
   keywords: appConfig.keywords,
 })
+
+const servicesOptions = [
+  { label: 'Web Design / Development', value: 'webdev' },
+  { label: 'Embedded Projects', value: 'embedded' },
+  { label: 'Backend', value: 'backend' },
+  { label: 'SEO', value: 'seo' },
+  { label: 'Other', value: 'other' },
+];
 </script>
