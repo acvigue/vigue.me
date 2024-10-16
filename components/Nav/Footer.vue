5022a1c1b48e0955ea71d85c56c2cb7f0c557402
Aiden Vigue <aiden@vigue.me>
2024-10-16 16:52:21 -0400

    remove spotify

--- components/Nav/Footer.vue
+++ components/Nav/Footer.vue
@@ -15,9 +15,6 @@ const currentYear = new Date().getFullYear()
       <NuxtLink to="https://github.com/acvigue" data-cursor-stick>
         <github class="stroke-persian text-transparent fill-transparent icon !h-7 !w-7" />
       </NuxtLink>
-      <NuxtLink to="https://open.spotify.com/user/w3g33ot2c4zxlxtzy15i7ldxi?si=bbdc95da1a284d60" data-cursor-stick>
-        <music class="stroke-persian text-transparent fill-transparent icon !h-7 !w-7" />
-      </NuxtLink>
     </div>
     <span class="text-persian text-lg font-serif2">© {{ currentYear }} Aiden Vigue</span>
   </div>
